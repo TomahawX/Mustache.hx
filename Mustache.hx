0019e660023b357ef4c95b222e1c88e6777b06d8
David Corticchiato <david.corticchiato@gmail.com>
2013-09-02 22:29:26 +0200

    manage arrays

--- Mustache.hx
+++ Mustache.hx
@@ -4,17 +4,26 @@ class Mustache {
     private var data:Dynamic;
     private var context:Dynamic;
     private var stack:Array<Dynamic>;
+    private var replacing:Bool;
 
     private function new(template:String, data:Dynamic) {
         this.template = new String(template);
         this.data = data;
         this.context = data;
         this.stack = [];
+        this.replacing = true;
     }
 
     static function main() {
-        var data:Dynamic = {dude: {name: 'john', 'age': 30, 'info': {situation: 'Married'}}, message: 'How are you ?'};
-        trace(Mustache.render("My name is {{#dude}}{{name}} and I'm {{age}}. I'm {{#info}}{{situation}}{{/info}}{{/dude}}. {{message}}", data));
+        var data:Dynamic = {
+            dudes: [
+                {name: 'john', 'age': 30, 'info': {situation: 'Married'}},
+                {name: 'charles', 'age': 25, 'info': {situation: 'Single'}}
+            ],
+            message: 'How are you ?'
+        };
+
+        trace(Mustache.render("{{#dudes}}\nMy name is {{name}} and I'm {{age}}. I'm {{#info}}{{situation}}{{/info}}{{/dudes}}. {{message}}", data));
     }
 
     static function render(template:String, data:Dynamic):String {
@@ -24,6 +33,7 @@ class Mustache {
 
     private function _render():String {
         var canReplace:Bool = true;
+
         while(canReplace){
             canReplace = _replaceNext();
         }
@@ -41,10 +51,33 @@ class Mustache {
         return value;
     }
 
+    private function _replaceArray(array:Array<Dynamic>, beginPattern:EReg, endPattern:EReg) {
+        var beginPosition:Dynamic = beginPattern.matchedPos();
+        var endPosition:Dynamic = endPattern.matchedPos();
+        var beforeArray:String = template.substring(0, beginPosition.pos);
+        var afterArray:String = template.substring(endPosition.pos + endPosition.len, template.length - 1);
+        var arrayString:String = template.substring(beginPosition.pos + beginPosition.len, endPosition.pos);
+        var value:String = '';
+
+        for (element in array) {
+            value += Mustache.render(arrayString, element);
+        }
+
+        template = beforeArray + value + afterArray;
+    }
+
     private function _replaceBlockOpener(pattern:EReg) {
         var expression:String = pattern.matched(3);
         
-        if (Reflect.hasField(context, expression) && Reflect.isObject(Reflect.getProperty(context, expression))) {
+        if (Std.is(Reflect.getProperty(context, expression), Array)) {
+            replacing = false;
+            var endPattern:EReg = new EReg("{{/" + expression + "}}", 'g');
+            if (endPattern.match(template)) {
+                _replaceArray(Reflect.getProperty(context, expression), pattern, endPattern);
+            } else {
+                replacing = true;
+            }
+        } else if (Reflect.isObject(Reflect.getProperty(context, expression))) {
             stack.push({name: expression, context: context});
             context = Reflect.getProperty(context, expression);
         }
@@ -60,17 +93,19 @@ class Mustache {
 
     private function _replacePattern(pattern:EReg) {
         var value:String = '';
-        var fullExpression:String = pattern.matched(2);
+        var tag:String = pattern.matched(2);
+        replacing = true;
 
-        if (fullExpression.length == 0) {
+        if (tag.length == 0) {
             value = _replaceVariable(pattern);
-        } else if (fullExpression == '#') {
+        } else if (tag == '#') {
             _replaceBlockOpener(pattern);
-        } else if (fullExpression == '/') {
+        } else if (tag == '/') {
             _replaceBlockCloser(pattern);
         }
-
-        template = pattern.replace(template, value);
+        if (replacing) {
+            template = pattern.replace(template, value);
+        }
     }
 
     private function _replaceNext():Bool {
